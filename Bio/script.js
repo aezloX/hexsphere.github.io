365b6ab9b8bf70a5f4fe9048cdccbf6e6feea237
Hexsphere <ethanhanderson1@gmail.com>
2017-02-06 11:54:56 -0700

    Moved Age Script to script.js

---
+++ Bio/script.js
@@ -0,0 +1,17 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+    window.onload = function() {
+        var currentYear = new Date().getFullYear();
+        var currentMonth = new Date().getMonth() + 1;
+        var currentDate = new Date().getDate();
+        var birthYear = 2004;
+        var month = 1;
+        var date = 23;
+        var age = currentYear - birthYear;
+
+        if ((-1 < month - currentMonth || (month === currentMonth && date < currentDate))) {
+                age = age - 1;
+        }
+        
+        document.getElementById('age').innerHTML = age;
+};
